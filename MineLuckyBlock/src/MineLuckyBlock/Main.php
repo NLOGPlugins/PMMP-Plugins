596b4a7b1bdbf55d57ceb3c2751ebeb38991a28a
ParkChanSol <sorisem4106@naver.com>
2016-10-20 16:27:53 +0900

    Update Main.php

--- MineLuckyBlock/src/MineLuckyBlock/Main.php
+++ MineLuckyBlock/src/MineLuckyBlock/Main.php
@@ -1,22 +1,24 @@
-﻿<?php
+<?php
 
-namespace RemoveEnchantItem;
+namespace MineLuckyBlock;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerItemHeldEvent;
 class Main extends PluginBase implements Listener {
-	public $config;
-	public $configdata;
+	
 	public function onLoad() {
 		$this->getLogger()->info(TextFormat::AQUA . "MineLuckyBlock 플러그인을 로드하고 있습니다");
 	}
+	
 	public function onEnable() {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getLogger()->info(TextFormat::AQUA . "MineLuckyBlock 플러그인을 사용할 수 있습니다!");
 	}
+	
 	public function onTouch(PlayerInteractEvent $event) {
 		$player = $this->getServer()->getPlayer();
 		$item = $this->getServer()->getPlayer()->getInventory()->getItem();
@@ -28,18 +30,26 @@ class Main extends PluginBase implements Listener {
 						$item->removeCreativeItem(369);
 					case "1":
 						$player->sendMessage("§b§o[알림] §7다시 한 번!");
-				}
-			}else{
-				$player->sendMessage("§b§o[알림] §7블레이즈 막대기를 엔더상자에 터치하세요");
+				}//mt_rand 종료
+			} //if (Block)문 종료
+			if (!($event->getBlock() == 369 )) {
+				$player->sendMessage("§b§o[알림] §7엔더상자에 터치해주세요");
 			}
-		}
+		}//if(held) 문 종료
 	}
+	
 	public function onBlock(BlockPlaceEvent $ev) {
 		if ($ev->getBlock()->getId() == 130) {
 			$ev->getPlayer()->sendMessage("§o§b[알림] §7당신은 이 블럭을 놓을 수 없습니다");
 			$ev->setCancelled(true);
-		}else{
-			
+			if ($ev->getPlayer()->isOp()) {
+				$ev->setCancelled(false);
+			}
+		}
+	}
+	public function onHold(PlayerItemHeldEvent $heldev) {
+		if ($heldev->getItem() == 130) {
+			$heldev->getItem()->setNamedTag("럭키블럭 열쇠");
 		}
 	}
 	public function onDisable() {
