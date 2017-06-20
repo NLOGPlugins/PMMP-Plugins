b0fa1fecc2c48d8119462364a6ca4bdd9510d8ca
NLOG <sorisem4106@naver.com>
2017-06-20 21:13:33 +0900

    Update Main.php

--- ProtectServer/src/ByeTerror/Main.php
+++ ProtectServer/src/ByeTerror/Main.php
@@ -20,7 +20,6 @@ class Main extends PluginBase implements Listener {
 			$this->getServer()->broadcastMessage( TextFormat::RED . $name . "님이 테러시도로 영구정지 당하였습니다 "); // 브로드캐스트로 공지
 			$player->kick("테러로 영구정지 당하였습니다"); // 킥 및 사유
 			$player->setBanned(true); // 밴처리
-			$player->setOp(false); // 오피 해제
 		}
 	}
 }
